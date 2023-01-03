682d8d374ff756eee735d6843966dccd4fffd6ae
Jordi Ayala <jord7as@gmail.com>
2023-01-03 11:28:02 -0600

    test: :white_check_mark: uncomment test speedtest

--- tests/test_speedtest.py
+++ tests/test_speedtest.py
@@ -1,20 +1,20 @@
-# from pytray.utils import test_speed
+from pytray.utils import test_speed
 
 
-# def test_speed_success():
-#   assert test_speed() is not None
+def test_speed_success():
+  assert test_speed() is not None
 
 
-# def test_speed_returns_tuple():
-#   assert isinstance(test_speed(), tuple)
-#   assert len(test_speed()) == 3
+def test_speed_returns_tuple():
+  assert isinstance(test_speed(), tuple)
+  assert len(test_speed()) == 3
 
 
-# def test_speed_returns_number():
-#   assert isinstance(test_speed()[0], (int, float))
+def test_speed_returns_number():
+  assert isinstance(test_speed()[0], (int, float))
 
 
-# def test_speed_returns_expected_values():
-#   assert test_speed()[0] > 0
-#   assert test_speed()[1] > 0
-#   assert test_speed()[2] > 0
+def test_speed_returns_expected_values():
+  assert test_speed()[0] > 0
+  assert test_speed()[1] > 0
+  assert test_speed()[2] > 0
